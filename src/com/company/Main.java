21671bdee214f046eb3d53780f0dbb31e79405fb
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-08-28 21:49:32 +0300

    add StringJoin class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String indent = scanner.nextLine();
+        String str1 = new String("1");
+        String str2 = new String("2");
+        String str3 = new String("3");
+        String str4 = StringJoin.join(indent, str1, str2, str3);
+        System.out.println(str4);
     }
 }
---
+++ src/com/company/StringJoin.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class StringJoin {
+    /**
+     * @param indent - spaces between string;
+     * @param str    - vararg, array from string;
+     */
+    public static String join(String indent, String... str) {
+        String str1 = new String();
+        for (int i = 0; i < str.length; i++) {
+            str1 += str[i] + indent;
+        }
+        return str1;
+    }
+}
