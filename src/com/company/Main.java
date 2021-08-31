4d1c0387ac6771e13fd38df17aab7ab92df1ff78
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-08-31 13:23:52 +0300

    add StringFormat method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter space between string");
         String indent = scanner.nextLine();
         String str1 = new String("1");
         String str2 = new String("2");
         String str3 = new String("3");
         String str4 = StringJoin.join(indent, str1, str2, str3);
         System.out.println(str4);
+        System.out.println("Enter first word");
+        String oneWord = scanner.nextLine();
+        System.out.println("Enter second word");
+        String twoWord = scanner.nextLine();
+        System.out.println("Enter three word");
+        String threeWord = scanner.nextLine();
+        String str5 = new String("Hi %s. How old are you? %s. Would you like a drink? %s");
+        String str6 = StringFormat.format(str5, oneWord, twoWord, threeWord);
+        System.out.println(str6);
     }
 }
---
+++ src/com/company/StringFormat.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class StringFormat {
+    /**
+     * @param str5 - text for edit;
+     * @param word - word, which need enter in "%s";
+     */
+    public static String format(String str5, String... word) {
+        char[] arrayChar = str5.toCharArray();
+        String string = new String();
+        int z = 0;
+        for (int i = 0; i < word.length; i++) {
+            for (int j = 0; j < arrayChar.length; j++) {
+                if (arrayChar[j] == '%' && arrayChar[j + 1] == 's') {
+                    arrayChar[j] = '0';
+                    arrayChar[j + 1] = '0';
+                    String charToString = String.valueOf(arrayChar);
+                    string += charToString.substring(z, j) + word[i];
+                    z = j + 2;
+                    break;
+                }
+            }
+        }
+        return string;
+    }
+}
+
