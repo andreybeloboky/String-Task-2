67b3a87e20877d88aa8dd6d12418bd0bc2c07ea4
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-13 00:31:15 +0300

    edit cycle for on the cycle while in the StringSplit class

--- src/com/company/StringSplit.java
+++ src/com/company/StringSplit.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class StringSplit {
     /**
-     * @param str     - text for edit;
+     * @param str       - text for edit;
      * @param delimiter - delimiter between different index in String array;
      * @return - string array with delimiter;
      */
@@ -11,24 +11,24 @@ public class StringSplit {
         char[] delimiterToChar = delimiter.toCharArray();
         int size = sizeArray(stringToChar, delimiterToChar);
         int begin = 0;
+        int j = 0;
         String[] str2 = new String[size + 1];
-        for (int i = 0; i < str2.length; i++) {
-            for (int j = 0; j < stringToChar.length; j++) {
+        for (int i = 0; i < str2.length - 1; i++) {
+            do {
                 if (stringToChar[j] == delimiterToChar[0]) {
                     stringToChar[j] = '0';
                     str2[i] = str.substring(begin, j);
                     begin = j + 2;
-                    break;
-                } else {
-                    str2[i] = str.substring(begin, stringToChar.length);
                 }
-            }
+                j++;
+            }while (stringToChar[j - 1] != '0');
         }
+        str2[str2.length - 1] = str.substring(begin, stringToChar.length);
         return str2;
     }
 
     /**
-     * @param stringToChar - translate string str11 to array char;
+     * @param stringToChar    - translate string str11 to array char;
      * @param delimiterToChar - translate string delimiter to array char delimiterToChar;
      * @return - size array string;
      */
