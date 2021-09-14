5510b88065373800168574e5172635640eca388b
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-14 23:49:30 +0300

    delete braces

--- src/com/company/StringIsEmpty.java
+++ src/com/company/StringIsEmpty.java
@@ -5,8 +5,8 @@ public class StringIsEmpty {
      * @param str - users text;
      * @return if string have 0 elements enter true and vice versa;
      */
-    public static boolean isEmpty(String str){
-        return (str.length() == 0) ? true : false;
+    public static boolean isEmpty(String str) {
+        return str.length() == 0 ? true : false;
     }
 }
 
