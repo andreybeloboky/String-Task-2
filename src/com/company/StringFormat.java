03449c51dcd6a5ea1c9f5d22d22f1ec66f6fc943
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-15 18:28:24 +0300

    create new method in StringFormat class

--- src/com/company/StringFormat.java
+++ src/com/company/StringFormat.java
@@ -2,28 +2,47 @@ package com.company;
 
 public class StringFormat {
     /**
-     * @param str  - text for edit;
-     * @param word - word, which need enter in "%s";
+     * @param str   - text for edit;
+     * @param words - word, which need enter in "%s";
      * @return - return string with replace "%s" onto user word;
      */
-    public static String format(String str, String... word) {
+    public static String format(String str, String... words) {
         char[] arrayChar = str.toCharArray();
         String string = new String();
-        String charToString = String.valueOf(arrayChar);
-        int z = 0;
-        int j = 0;
-        for (String s : word) {
+        int startFragmentIndex = 0;
+        int endFragmentIndex = 0;
+        for (String word : words) {
             do {
-                if (arrayChar[j] == '%' && arrayChar[j + 1] == 's') {
-                    arrayChar[j] = '0';
-                    arrayChar[j + 1] = '0';
-                    string += charToString.substring(z, j) + s;
-                    z = j + 2;
+                if (arrayChar[endFragmentIndex] == '%' && arrayChar[endFragmentIndex + 1] == 's') {
+                    string += substring(arrayChar, endFragmentIndex, startFragmentIndex, string, str) + word;
+                    startFragmentIndex = endFragmentIndex + 2;
                 }
-                j++;
-            } while (arrayChar[j] != '0' && arrayChar[j + 1] != '0');
+                endFragmentIndex++;
+            } while (arrayChar[endFragmentIndex] != '0' && arrayChar[endFragmentIndex + 1] != '0');
         }
         return string;
     }
-}
 
+    /**
+     * @param arrayChar        - edit string to char;
+     * @param endFragmentIndex - end index in char array before the symbal %s;
+     */
+    public static void replaceChar(char[] arrayChar, int endFragmentIndex) {
+        arrayChar[endFragmentIndex] = '0';
+        arrayChar[endFragmentIndex + 1] = '0';
+    }
+
+    /**
+     * @param arrayChar          - edit string to char;
+     * @param endFragmentIndex   - end index in char array before the symbal %s;
+     * @param startFragmentIndex - from begin array to endFragmentIndex
+     * @param string             - new string for result recording;
+     * @param str                - text for edit;
+     * @return - return string with replace "%s" onto user word;
+     */
+    public static String substring(char[] arrayChar, int endFragmentIndex, int startFragmentIndex, String string, String str) {
+        replaceChar(arrayChar, endFragmentIndex);
+        string = str.substring(startFragmentIndex, endFragmentIndex);
+        return string;
+    }
+}
